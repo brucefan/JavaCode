3c3f4150312d5537bcc96b60a0794876b4ad31b1
brucefan <277473729@qq.com>
2015-12-06 00:57:06 +0800

    add

---
+++ java8/src/main/java/com/brucefan/code/concurrent/requestLimit.java
@@ -0,0 +1,8 @@
+package com.brucefan.code.concurrent;
+
+/**
+ * 现有一个接口，他的极限只能承受100个并发访问，当前有1000个并发过来，如何才能让这个接口正常的运作呢？
+ * Created by Administrator on 2015/12/6.
+ */
+public class requestLimit {
+}
